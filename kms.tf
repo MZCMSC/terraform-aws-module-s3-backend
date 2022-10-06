0a4709d2d3ce2d33c6597b9d718ab4b965f0e593
MZCMSC <ygcho@mz.co.kr>
2022-10-06 15:36:29 +0900

    All delete

--- kms.tf
+++
@@ -1,8 +0,0 @@
-# Terraform AWS Module S3
-################################
-# KMS
-################################
-resource "aws_kms_key" "this" {
-    description = "AWS KMS for Terraform-satate-backend S3 Bucket"
-    deletion_window_in_days = 10 
-}
